3861475d9454aee7a49dcb18a5abcdca0fa20b77
rogelioRa <roger_17@live.com>
2016-12-14 12:55:30 -0600

    cambios roger

---
+++ app/views/child/init.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="/packages/images/landing/logo.png">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="packages/libs/materialize/css/materialize.min.css">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="/packages/css/skins/{{--{{User::getSkin()->skin}}.css--}}">
+    <link rel="stylesheet" type="text/css" href="packages/assets/css/child/init.css">
+    @yield('mi_css')
+    <title>Curiosity | @yield('title')</title>
+  </head>
+  <body class="hold-transition {{-- {{User::getSkin()->skin}} --}} sidebar-mini sidebar-collapse">
+    <div class="container">
+        <div class="row">
+          <div class="col s12">
+            <img src="packages/assets/media/images/child/nav1.png" class="img-responsive card img-nav">
+          </div>
+        </div>
+    </div>
+    </div>
+  </body>
+  <script type="text/javascript" src="packages/libs/materialize/js/materializa.js"></script>
+</html>
