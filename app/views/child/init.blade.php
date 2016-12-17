2d94c61bc2d9b765646105b20182294c4234a7c1
rogelioRa <roger_17@live.com>
2016-12-17 13:19:24 -0600

    Cambios vista inicio

--- app/views/child/init.blade.php
+++ app/views/child/init.blade.php
@@ -15,46 +15,62 @@
           </div>
           <!--// find de sección del slide principal -->
           <!-- sección de zona de paneles-->
+          <div class="panels">
+            <div class="row">
+              <div class="col m6 s6 x6">
+                  <div class="card blue-grey darken-1 z-depth-2" id="card-news-games">
+                    <div class="card-content white-text text-center card-mask">
+                      <span class="card-title">Juegos nuevos</span>
+                      <hr>
+                      <a class="btn btn-view-more">Ver</a>
+                    </div>
+                  </div>
+              </div>
+              <div class="col m6 s6 x6">
+                 <div class="card blue-grey darken-1 z-depth-2" id="card-games-populars">
+                    <div class="card-content white-text text-center card-mask">
+                      <span class="card-title">Juegos populares</span>
+                      <hr>
+                      <a class="btn btn-view-more">Ver</a>
+                    </div>
+                  </div>
+              </div>
+            </div>
+             <div class="row">
+              <div class="col m6 s6 x6 standard-bordered">
+                  <div class="card blue-grey darken-1 depth-2" id="card-more-calification">
+                    <div class="card-content white-text text-center card-mask">
+                      <span class="card-title">Juegos mejor calificados</span>
+                      <hr>
+                      <a class="btn btn-view-more">Ver</a>
+                    </div>
+                  </div>
+              </div>
+              <div class="col m6 s6 x6 standard-bordered">
+                 <div class="card blue-grey darken-1 depth-2" id="card-games-recoments">
+                    <div class="card-content white-text text-center card-mask">
+                      <span class="card-title"><i class="tiny material-icons insert_chart"></i> Juegos recomendados</span>
+                      <hr>
+                      <a class="btn btn-view-more">Ver</a>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <!-- Carta donde se mostrará el contenido de los juegos(juegos recomendaods..etc) -->
           <div class="row">
-            <div class="col m6 s6 x6">
-                <div class="card blue-grey darken-1 z-depth-2" id="card-news-games">
-                  <div class="card-content white-text text-center card-mask">
-                    <span class="card-title">Juegos nuevos</span>
-                    <hr>
-                    <a class="btn btn-view-more">Ver</a>
-                  </div>
+            <div class="col s12 m12">
+              <div class="card blue-grey darken-1" id="card-container-games">
+                <div class="card-content white-text">
+                  
                 </div>
-            </div>
-            <div class="col m6 s6 x6">
-               <div class="card blue-grey darken-1 z-depth-2" id="card-games-populars">
-                  <div class="card-content white-text text-center card-mask">
-                    <span class="card-title">Juegos populares</span>
-                    <hr>
-                    <a class="btn btn-view-more">Ver</a>
-                  </div>
+                <div class="card-action">
+                  
                 </div>
+              </div>
             </div>
           </div>
-           <div class="row">
-            <div class="col m6 s6 x6 standard-bordered">
-                <div class="card blue-grey darken-1 depth-2" id="card-more-calification">
-                  <div class="card-content white-text text-center card-mask">
-                    <span class="card-title">Juegos mejor calificados</span>
-                    <hr>
-                    <a class="btn btn-view-more">Ver</a>
-                  </div>
-                </div>
-            </div>
-            <div class="col m6 s6 x6 standard-bordered">
-               <div class="card blue-grey darken-1 depth-2" id="card-games-recoments">
-                  <div class="card-content white-text text-center card-mask">
-                    <span class="card-title"><i class="tiny material-icons insert_chart"></i> Juegos recomendados</span>
-                    <hr>
-                    <a class="btn btn-view-more">Ver</a>
-                  </div>
-                </div>
-            </div>
-          </div>
+          <!-- // Carta donde se mostrará el contenido de los juegos(juegos recomendaods..etc) -->
           <div class="row">
             <div class="col m12  s12 x12 standard-bordered">
                 <div class="card blue-grey darken-1 depth-2" id="card-news-videos">
@@ -66,6 +82,7 @@
                 </div>
             </div>
           </div>
+
           <!-- Fin de la zona de paneles-->
       </div>
     @stop
--- public/packages/assets/js/child/init.js
+++ public/packages/assets/js/child/init.js
@@ -3,7 +3,10 @@ addEvent("click",document.getElementsByClassName("btn-view-more"));
 function addEvent(nameEvent,elements){
 	for(var i=0;i<elements.length;i++){
 		elements[i].addEventListener(nameEvent,function(evt){
-			evt.target.setAttribute("class",evt.target.getAttribute("class")+" hide-efect");
+			var cardContenido = document.getElementById("card-container-games");//obtenemos la tarjeta donde mostraremos el contenido de los juegos
+			cardContenido.setAttribute("class",cardContenido.getAttribute("class")+" show-efect");
+			var containerCards = evt.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+			containerCards.setAttribute("class",containerCards.getAttribute("class")+" hide-efect");
 		},false);
 	}
 }
