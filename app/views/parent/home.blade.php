a2d2b0aed11dfc1596bac6810da3ebf99591fbe1
wilvardo <wilvardo@gmail.com>
2016-12-19 22:50:28 -0600

    Integracion completa de carousel

--- app/views/parent/home.blade.php
+++ app/views/parent/home.blade.php
@@ -1,95 +1,7 @@
 @extends('templates.parent-menu')
 
 @section('css-plus')
-<style media="screen">
-.carousel {
-  overflow: hidden;
-  position: relative;
-  width: 100%;
-  height: 400px;
-  -webkit-perspective: 500px;
-          perspective: 500px;
-  -webkit-transform-style: preserve-3d;
-          transform-style: preserve-3d;
-  -webkit-transform-origin: 0% 50%;
-          transform-origin: 0% 50%;
-}
-
-.carousel.carousel-slider {
-  top: 0;
-  left: 0;
-  height: 0;
-}
-
-.carousel.carousel-slider .carousel-fixed-item {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 20px;
-  z-index: 1;
-}
-
-.carousel.carousel-slider .carousel-fixed-item.with-indicators {
-  bottom: 68px;
-}
-
-.carousel.carousel-slider .carousel-item {
-  width: 100%;
-  height: 100%;
-  min-height: 400px;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-
-.carousel.carousel-slider .carousel-item h2 {
-  font-size: 24px;
-  font-weight: 500;
-  line-height: 32px;
-}
-
-.carousel.carousel-slider .carousel-item p {
-  font-size: 15px;
-}
-
-.carousel .carousel-item {
-  display: none;
-  width: 200px;
-  height: 400px;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-
-.carousel .carousel-item img {
-  width: 100%;
-}
-
-.carousel .indicators {
-  position: absolute;
-  text-align: center;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  margin: 0;
-}
-
-.carousel .indicators .indicator-item {
-  display: inline-block;
-  position: relative;
-  cursor: pointer;
-  height: 8px;
-  width: 8px;
-  margin: 24px 4px;
-  background-color: rgba(255, 255, 255, 0.5);
-  transition: background-color .3s;
-  border-radius: 50%;
-}
-
-.carousel .indicators .indicator-item.active {
-  background-color: #fff;
-}
-</style>
+<link rel="stylesheet" href="/packages/libs/materialize/css/carousel.css">
 @stop
 
 @section('content')
@@ -134,9 +46,5 @@
 
 @section('js')
 <script src="/packages/libs/materialize/js/materialize.min.js" charset="utf-8"></script>
-<script type="text/javascript">
-   $(function(){
-      $(".carousel").carousel();
-   });
-</script>
+<script src="/packages/assets/js/parent/homeParent-main.js" charset="utf-8"></script>
 @stop
