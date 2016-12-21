0b4159646b83d16a23887825454c3ad5b58e719d
wilvardo <wilvardo@gmail.com>
2016-12-21 17:06:07 -0600

    commit

--- app/views/child/curiosity-store.blade.php
+++ app/views/child/curiosity-store.blade.php
@@ -4,12 +4,58 @@
 @stop
 
 @section('content')
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+   <div class="view hm-black-strong z-depth-1 col-xs-12" id="cst-banner">
+      <div class="mask flex-center">
+         <p class="white-text">Tienda Curiosity</p>
+      </div>
+   </div>
+
+   <div class="row">
+      <div class="col-sm-7" id="cst-boxOfAvatar">
+         <div class="cst-titleSection z-depth-1">
+            <span class="fa fa-user"></span>&nbsp;
+            Accesorios para tu avatar
+         </div>
+
+         <div class="cst-boxSlideItems z-depth-1">
+            <div class="view hm-black-strong z-depth-1 col-sm-4 cst-titleCarousel">
+               <div class="mask flex-center">
+                  <p class="white-text">Herramientas</p>
+               </div>
+            </div>
+            <div class="col-sm-8 z-depth-1 cst-panelSlide">
+            </div>
+         </div>
+
+         <div class="cst-boxSlideItems z-depth-1">
+            <div class="view hm-black-strong z-depth-1 col-sm-4 cst-titleCarousel">
+               <div class="mask flex-center">
+                  <p class="white-text">Herramientas</p>
+               </div>
+            </div>
+            <div class="col-sm-8 z-depth-1 cst-panelSlide">
+            </div>
+         </div>
+
+      </div>
+      <div class="col-sm-5" id="cst-boxOfProfile">
+         <div class="cst-titleSection z-depth-1">
+            <span class="fa fa-user"></span>&nbsp;
+            Mejora tu perfil
+         </div>
+
+         <div class="cst-boxSlideItems z-depth-1">
+            <div class="view hm-black-strong z-depth-1 col-sm-5 cst-titleCarousel">
+               <div class="mask flex-center">
+                  <p class="white-text">Color</p>
+               </div>
+            </div>
+            <div class="col-sm-7 z-depth-1 cst-panelSlide">
+            </div>
+         </div>
+
+      </div>
+   </div>
 @stop
 
 @section('js-plus')
