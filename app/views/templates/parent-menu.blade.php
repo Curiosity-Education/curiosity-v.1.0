3ae28ef87611b29cc2f97742e580e7efcd5f201c
wilvardo <wilvardo@gmail.com>
2016-12-26 13:09:58 -0600

    terminando menu de padres

--- app/views/templates/parent-menu.blade.php
+++ app/views/templates/parent-menu.blade.php
@@ -13,37 +13,41 @@
 
 @section('menu-links')
 <div data-url="" class="linkMenu linkMenu-active">
-   <span class="fa fa-home icon-menu" id="parentMenu-icon-home"></span>&nbsp;
+   <span class="fa fa-home" id="parentMenu-icon-home"></span>&nbsp;
    Inicio
 </div>
 <div data-url="" class="linkMenu">
-   <span class="fa fa-youtube-play icon-menu" id="parentMenu-icon-videos"></span>&nbsp;
+   <span class="fa fa-youtube-play" id="parentMenu-icon-videos"></span>&nbsp;
    Biblioteca de videos
 </div>
 <div data-url="" class="linkMenu">
-   <span class="fa fa-youtube-play icon-menu" id="parentMenu-icon-files"></span>&nbsp;
+   <span class="fa fa-youtube-play" id="parentMenu-icon-files"></span>&nbsp;
    Biblioteca de archivos
 </div>
 @stop
 
 @section('menu-links-aside')
 <div data-url="" class="linkMenu linkMenuAside linkMenu-active">
-   <span class="fa fa-home icon-menu" id="parentMenu-icon-home"></span>&nbsp;
+   <span class="fa fa-home" id="parentMenu-icon-home"></span>&nbsp;
    Inicio
 </div>
 <div data-url="" class="linkMenu linkMenuAside">
-   <span class="fa fa-youtube-play icon-menu" id="parentMenu-icon-videos"></span>&nbsp;
+   <span class="fa fa-youtube-play" id="parentMenu-icon-videos"></span>&nbsp;
    Biblioteca de videos
 </div>
 <div data-url="" class="linkMenu linkMenuAside">
-   <span class="fa fa-file-pdf-o icon-menu" id="parentMenu-icon-files"></span>&nbsp;
+   <span class="fa fa-file-pdf-o" id="parentMenu-icon-files"></span>&nbsp;
    Biblioteca de archivos
+</div>
+<div class="linkMenu linkMenuAside" id="parentMenu-addChild-aside">
+   <span class="fa fa-plus-circle"></span>&nbsp;
+   Registrar Hijo
 </div>
 @stop
 
 @section('under-menu')
 <div id="parentMenu-addChild" class="z-depth-1">
-   <span class="fa fa-plus-circle icon-menu"></span>&nbsp;
+   <span class="fa fa-plus-circle"></span>&nbsp;
    Registrar Hijo
 </div>
 @stop
