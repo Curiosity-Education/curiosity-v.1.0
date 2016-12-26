d7477fa3f34aa452538953a3432f8e4a5d5f1b1c
gerson22 <gerson.isaias.rivera@gmail.com>
2016-12-26 14:17:48 -0600

    Se corrigio el problema

--- app/ext/ExternalCall.php
+++ app/ext/ExternalCall.php
@@ -1,4 +1,4 @@
-<?
+<?php
 class ExternalCall{
 
     private static $_externalCall;
@@ -20,13 +20,14 @@ class ExternalCall{
 
     public static function execute($callback = NULL){
         if(is_callable($callback))
-            return call_user_func_array($callback, array_slice($this->matches, 1));
+            return call_user_func_array($callback, array_slice(["GET","POST"], 1));
         else{
             $callbackStr = explode('@',$callback);
             $controller = new $callbackStr[0];
 
-            return call_user_func_array(array($controller,$callbackStr[1]),array_slice($this->matches, 1));
+            return call_user_func_array(array($controller,$callbackStr[1]),array_slice(["GET","POST"], 1));
 
         }
     }
 }
+
