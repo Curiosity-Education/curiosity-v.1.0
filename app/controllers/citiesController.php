3054e94411f46a0d5bd51d1f5ffe7811837f95c1
gerson22 <gerson.isaias.rivera@gmail.com>
2016-12-27 12:47:23 -0600

    Se actualizo controlador citiesController

--- app/controllers/citiesController.php
+++ app/controllers/citiesController.php
@@ -1,8 +1,10 @@
 <?php 
 class citiesController extends BaseController{
 	
-	function get(){
-		
+	public function get(){
+		$state_id = Input::get('estado_id');
+		$cities = ciudad::where("estado_id","=",$state_id)->get();
+		return $cities;
 	}
 	function save(){
 		
