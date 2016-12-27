32653e10f5051d4afa1b625f3aaf9b841772e894
gerson22 <gerson.isaias.rivera@gmail.com>
2016-12-27 13:48:08 -0600

    Se agrego la función update al userController

--- app/controllers/usersController.php
+++ app/controllers/usersController.php
@@ -68,7 +68,62 @@ class usersController extends BaseController(){
         }
 	}
 	function update(){
-		
+		$data  =    Input::get('data');
+        $dateNow = date("Y-m-d");
+        if(!Auth::User()->hasRole('hijo') && !Auth::User()->hasRole('hijo_free') && !Auth::User()->hasRole('demo_hijo')){
+            $date_min =strtotime("-18 year",strtotime($dateNow));
+        }else{
+             $date_min =strtotime("-4 year",strtotime($dateNow));
+        }
+        $date_min=date("Y-m-d",$date_min);
+        $rules= [
+            "username_persona"          =>"required|user_check|max:50",
+            "nombre_persona"            =>"required|letter|max:50",
+            "apellido_paterno_persona"  =>"required|letter|max:30",
+            "apellido_materno_persona"  =>"required|letter|max:30",
+            "sexo_persona"              =>"required|string|size:1",
+            "fecha_nacimiento_persona"  =>"required|date_format:Y-m-d|before:$date_min"
+        ];
+        $messages = [
+               "required"    =>  "Este campo :attribute es requerido",
+               "alpha"       =>  "Solo puedes ingresar letras",
+               "before"      =>  "La fecha que ingresaste tiene que ser menor al $date_min",
+               "date"        =>  "Formato de fecha invalido",
+               "numeric"     =>  "Solo se permiten digitos",
+               "email"       =>  "Ingresa un formato de correo valido",
+               "unique"      =>  "Este usuario ya existe",
+               "integer"     =>  "Solo se permiten numeros enteros",
+               "exists"      =>  "El campo :attribute no existe en el sistema",
+               "unique"      =>  "El campo :attribute no esta disponible intente con otro valor",
+               "integer"     =>  "Solo puedes ingresar numeros enteros",
+               "same"        =>  "Las contraseñas no coinciden",
+               "after"       =>  "La fecha de expiracion es incorrecta, no puedes ingresar fechas inferiores al día de hoy",
+         ];
+        $valid = Validator::make($datos,$rules,$messages);
+        if($valid->fails()){
+            return $valid->messages();
+        }else{
+
+            $user =User::find(Auth::user()->id);
+            $data["username"]=$data["username_persona"];
+            $user->update($data);
+            $person= $user->persona();
+            $data_person = [
+                "nombre"            =>  $datos["nombre_persona"],
+                "apellido_paterno"  =>  $datos["apellido_paterno_persona"],
+                "apellido_materno"  =>  $datos["apellido_materno_persona"],
+                "sexo"              =>  $datos["sexo_persona"],
+                "fecha_nacimiento"  =>  $datos["fecha_nacimiento_persona"]
+            ];
+            $person->update($data_person);
+            $rolee = Auth::user()->roles[0]->name;
+            if ($rolee == "padre" || $rolee == "padre_free" || $rolee == "demo_padre"){
+                $dadId = Auth::user()->persona()->first()->padre()->first()->id;
+                $dad = padre::where('id', '=', $dadId)->first();
+                $dad->update($data);
+            }
+            return Response::json("bien");
+        }
 	}
 	function delete(){
 		
