09fa0ad4975dea5be752621d4faf5e7d717e79af
gerson22 <gerson.isaias.rivera@gmail.com>
2016-12-27 12:59:05 -0600

    Controladores de citiesController y statesController han iniciado actualización

--- app/controllers/citiesController.php
+++ app/controllers/citiesController.php
@@ -1,11 +1,20 @@
 <?php 
 class citiesController extends BaseController{
 	
-	public function get(){
-		$state_id = Input::get('estado_id');
-		$cities = ciudad::where("estado_id","=",$state_id)->get();
-		return $cities;
+	public function all(){
+
 	}
+    public function get(){
+        $estadoId = Input::get('estado');
+        $pais = Input::get('pais');
+        $query = DB::table('ciudades')->join('estados', 'estado_id', '=', 'estados.id')
+        ->join('paises', 'pais_id', '=', 'paises.id')
+        ->where('estados.id', '=', $estadoId)
+        ->where('paises.pais', '=', $pais)
+        ->select('ciudades.*')
+        ->get();
+        return $query;
+    }
 	function save(){
 		
 	}
--- app/controllers/statesController.php
+++ app/controllers/statesController.php
@@ -2,7 +2,7 @@
 class statesController extends BaseController{
 	
 	function get(){
-		
+        return estado::where('pais_id', '=', pais::where('pais', '=', $pais)->pluck('id'))->get();
 	}
 	function save(){
 		
