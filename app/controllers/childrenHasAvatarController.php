e945909f5b17eff246d2cc799100e40658a194b8
gerson22 <gerson.isaias.rivera@gmail.com>
2016-12-27 12:18:59 -0600

    Se creo un nuevo controlador que se llama childrenHasAvatarController

---
+++ app/controllers/childrenHasAvatarController.php
@@ -0,0 +1,17 @@
+<?php
+
+class childrenHasAvatarController extends BaseController{
+
+    public function save(){
+        $av = Input::get('data');
+        $myId = Auth::User()->persona()->first()->hijo()->pluck('id');
+        DB::table('hijos_avatars')->insert(array(
+            'hijo_id' => $myId,
+            'avatar_id' => $av
+        ));
+        $us = Auth::user();
+        $us->flag = 0;
+        $us->save();
+        return Response::json(array("status" => 200, "statusMessage" => "success"));
+    }
+}
