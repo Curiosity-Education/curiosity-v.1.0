98bd76cfac24d03ebe4d644dc656e6480baf10db
wilvardo <wilvardo@gmail.com>
2016-12-31 12:18:19 -0600

    Preparando los controladores para modulo de gestion de contenido

--- app/controllers/topicsController.php
+++ app/controllers/topicsController.php
@@ -1,6 +1,26 @@
 <?php
 class topicsController extends BaseController{
 
+	function all(){
+		$topics = Topic::where('active', '=', 1)->get();
+		return $topics;
+	}
+
+	function getWithActivities(){
+		$topics = Activity::join('temas', 'actividades.tema_id', '=', 'temas.id')
+		->join('bloques', 'temas.bloque_id', '=', 'bloques.id')
+		->join('inteligencias', 'bloques.inteligencia_id', '=', 'inteligencias.id')
+		->join('niveles', 'inteligencias.nivel_id', '=', 'niveles.id')
+		->where('actividades.active', '=', 1)
+		->where('temas.active', '=', 1)
+		->where('bloques.active', '=', 1)
+		->where('inteligencias.active', '=', 1)
+		->where('niveles.active', '=', 1)
+		->where('actividades.estatus', '=', 'eneabled')
+		->select('temas.*')->get();
+		return $topics;
+	}
+
 	function save(){
 		$data = Input::get('data');
 		$rules = array(
