188d274f255268ab779279f4448164af0328b8eb
gerson22 <gerson.isaias.rivera@gmail.com>
2017-01-09 10:50:57 -0600

    Se sube lo multilenguaje

---
+++ app/lang/es/landingPage.php
@@ -0,0 +1,14 @@
+<?php
+ return array(
+
+    'eslogan' =>'¡Que tu curiosidad no tenga límites!',
+    'menu'=>array(
+        'home' => 'Inicio',
+        'whatIs' => '¿Qué es Curiosity?',
+        'paymentOption' => 'Membresía',
+        'howfunction' => '¿Cómo funciona?',
+        'video' => 'Video',
+        'createAccount' => 'Registrarse',
+        'logIn' => 'Iniciar Sesión'
+    )
+ );
