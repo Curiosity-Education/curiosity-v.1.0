55d1a84627418a887679c7ffc536155dbd2ec6f9
canales95 <fernando.canales@curiosity.com.mx>
2017-01-17 16:19:50 -0600

    más unificación vistas del papá

--- app/views/parent/childHelp.blade.php
+++ app/views/parent/childHelp.blade.php
@@ -14,51 +14,51 @@
 			<section class="chp-help">
 			  <div class="">
 				<div class="row">
-				  <div class="col-sm-12 col-md-4">
+				  <div class="col-sm-6 col-md-4">
 						<!--Panel-->
 						<div class="card chp-cards chp-Weak-card">
-							<div class="card-header chp-backColor white-text">
-								<i class="fa fa-user-circle-o"></i>&nbsp; Temas debiles de Juan
+							<div class="card-header chp-backColor white-text text-left">
+								<i class="fa fa-eye"></i>&nbsp; Temas débiles de Juan
 							</div>
-							<div class="card-block">
+							<div class="card-block" id="chp-contentTopics">
 							<ul>
 								<li>
-									<div class="card hoverable chp-topics">
-									<div class="card-block chp-topic-card">
-									  <div class="card-left">
-										<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" class="chp-imgWeak">
-									  </div>
-									  <div class="card-right">
-										<div class="chp-topicDesc">
-										  <p>Fracciones</p>
+									<div class="card hoverable chp-topics chp-cardTopic chp-active">
+										<div class="card-block chp-topic-card">
+										  <div class="card-left">
+											<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" class="chp-imgWeak z-depth-1">
+										  </div>
+										  <div class="card-right">
+											<div class="chp-topicDesc">
+											  <p>Fracciones</p>
+											</div>
+										  </div>
 										</div>
-									  </div>
-									</div>
 								  </div>
 								</li>
 								<li>
-								<div class="card hoverable chp-topics">
+								<div class="card hoverable chp-topics chp-cardTopic">
 									<div class="card-block chp-topic-card">
 									  <div class="card-left">
-										<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" class="chp-imgWeak">
+										<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" class="chp-imgWeak z-depth-1">
 									  </div>
 									  <div class="card-right">
 										<div class="chp-topicDesc">
-										  <p>Fracciones</p>
+										  <p>Sumas</p>
 										</div>
 									  </div>
 									</div>
 								 </div>
 								</li>
 								<li>
-									<div class="card hoverable chp-topics">
+									<div class="card hoverable chp-topics chp-cardTopic">
 										<div class="card-block chp-topic-card">
 										  <div class="card-left">
-											<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" class="chp-imgWeak">
+											<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" class="chp-imgWeak z-depth-1">
 										  </div>
 										  <div class="card-right">
 											<div class="chp-topicDesc">
-											  <p>Fracciones</p>
+											  <p>Multiplicaciones</p>
 											</div>
 										  </div>
 										</div>
@@ -69,42 +69,59 @@
 						</div>
 						<!--/.Panel-->
 				  </div>
-					<div class="col-sm-12 col-md-8">
-						<div class="card chp-cards">
+					<div class="col-sm-6 col-md-8">
+						<div class="card chp-cards" id="chp-contentMaterial">
 							<div class="card-header chp-backColor white-text">
-						  		<i class="fa fa-info"></i>&nbsp; Material para Fracciones
+						  		<i class="fa fa-cube"></i>&nbsp; Material para Fracciones
 							</div>
 						<div class="card-block">
-							<div class="col-sm-6 col-md-6">
+							<div class="col-sm-12 col-md-6">
 						  		<div class="chp-btn-mat">
-									<a class="btn btn-rounded chp-btn chp-btn-material">Primary</a>
-						  		</div>
+									<center>
+										<p>Click para ver la guía explicativa</p>
+										<a href="#">
+											<img src="packages/assets/media/images/parents/pdfs.png" alt="" class="chp-pdfImg">
+										</a>
+									</center>
+						  		</div><br>
 							</div>
-							<div class="col-sm-6 col-md-6 chp-border">
+							<div class="col-sm-12 col-md-6 chp-border chp-border-left">
 								<div class="text-xs-center">
-									<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" alt="" class=" chp-videoImg">
+									<center>
+										<p>Click para ver el video explicativo</p>
+										<a href="#">
+											<img src="packages/assets/media/images/parents/video.png" alt="" class="chp-videoImg">
+										</a>
+									</center>
 								</div>
 							</div>
 						</div>
 					</div>
 					</div>
+
 				  <div class="col-sm-12 col-md-8">
 					<div class="card chp-cards">
 						<div class="card-header chp-backColor white-text">
-						  <i class="fa fa-bookmark"></i>&nbsp; Material para Fracciones
+						  <i class="fa fa-bookmark"></i>&nbsp; Librerías SM te ofrece Material extra de refuerzo
 						</div>
 						<div class="card-block">
 							<div class="col-sm-4 col-md-4">
 								<div>
-									<img src="http://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg" alt="" class="chp-bookImg">
+									<center>
+										<img src="packages/assets/media/images/parents/documento.jpg" alt="" class="chp-bookImg">
+									</center>
 								</div>
 							</div>
 							<div class="col-md-4 col-sm-4">
-							  <p class="text-xs-center chp-book-desc">Lorem ipsum dolor sit amet, consectetur adipisicing el</p>
+						  	  <h3 class="h3-responsive text-xs-center">Fracciones</h3>
+							  <p class="text-xs-center chp-book-desc">4to. Grado</p>
+							  <p class="text-xs-center chp-book-desc">Librerías SM</p>
 							</div>
 							<div class="col-sm-4 col-md-4">
 								<div class="chp-btn-book">
-								  <a class="btn btn-rounded chp-btn chp-btn-bookshop">Primary</a>
+								  <center>
+								  	 <a class="btn btn-rounded chp-btn chp-btn-bookshop">Adquirir para Juan</a>
+								  </center>
 								</div>
 							</div>
 						</div>
