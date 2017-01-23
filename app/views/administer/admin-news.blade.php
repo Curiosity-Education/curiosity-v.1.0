2a7bf0981c87f140dfb541d40c78fca6e79d66ad
canales95 <fernando.canales@curiosity.com.mx>
2017-01-23 12:25:19 -0600

    a

---
+++ app/views/administer/admin-news.blade.php
@@ -0,0 +1,21 @@
+@extends('templates.administer-menu')
+
+@section('css-plus')
+@stop
+
+@section('baner-tittle')
+  Novedades
+@stop
+
+@section('content-administer')
+
+@stop
+
+@section('js-plus')
+   <script src="/packages/assets/js/administer/models/Level.js" charset="utf-8"></script>
+   <script src="/packages/assets/js/administer/models/Intelligence.js" charset="utf-8"></script>
+   <script src="/packages/assets/js/administer/models/Block.js" charset="utf-8"></script>
+   <script src="/packages/assets/js/administer/models/Topic.js" charset="utf-8"></script>
+   <script src="/packages/assets/js/administer/Controllers/atpController.js" charset="utf-8"></script>
+   <script src="/packages/assets/js/administer/dispatchers/dsp-atp.js" charset="utf-8"></script>
+@stop
