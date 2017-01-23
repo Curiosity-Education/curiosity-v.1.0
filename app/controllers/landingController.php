7b2fee1fa1fea1deee7279ec54c19153ad9641f2
gerson22 <gerson.isaias.rivera@gmail.com>
2017-01-23 03:54:21 -0600

    Ya carga los planes

--- app/controllers/landingController.php
+++ app/controllers/landingController.php
@@ -7,8 +7,17 @@ class landingController extends BaseController
 {
 
   function landingpage(){
-    $escuelas = array('escuelas' => School::where('active', '=', 1)->get());
-    	return View::make('landing.index', $escuelas);
+    $escuelas = School::where('active', '=', 1)->get();
+    $planes = Plan::all();
+    	return View::make('landing.index',array(
+            'escuelas' => $escuelas,
+            'planes'   => $planes,
+            'trans'    => [
+                'month' => 'mes',
+                'semester'  => 'semestre',
+                'year'  =>  'año'
+            ]
+        ));
   }
 
 }
