04321824fcc008237f13b3ee081a14de95a5bc9f
canales95 <fernando.canales@curiosity.com.mx>
2017-01-24 13:23:39 -0600

    mas cambios de novedades

--- app/views/administer/admin-news.blade.php
+++ app/views/administer/admin-news.blade.php
@@ -10,15 +10,18 @@
 @section('content-administer')
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-4 adNews-borderRight z-depth-1" id="adNews-form">
+		<div class="col-md-5 adNews-borderRight z-depth-1" id="adNews-form">
 			<h4 class="h4-responsive text-xs-left"><i class="fa fa-file-text"></i>&nbsp; Novedad</h4>
 			<hr>
 		</div>
-		<div class="col-md-8 adNews-content">
+		<div class="col-md-7 adNews-content">
 			<div class="row">
 				<div id="adNews-allnews" class="z-depth-1">
 					<h4 class="h4-responsive text-xs-left"><i class="fa fa-file-text"></i>&nbsp; Lista de Novedades</h4>
 					<hr>
+					<div id="adNews-contentNews">
+
+					</div>
 				</div>
 			</div>
 		</div>
@@ -28,6 +31,6 @@
 
 @section('js-plus')
    <script src="/packages/assets/js/administer/models/News.js" charset="utf-8"></script>
-   <script src="/packages/assets/js/administer/Controllers/newsController.js" charset="utf-8"></script>
+   <script src="/packages/assets/js/administer/controllers/newsController.js" charset="utf-8"></script>
    <script src="/packages/assets/js/administer/dispatchers/dsp-news.js" charset="utf-8"></script>
 @stop
