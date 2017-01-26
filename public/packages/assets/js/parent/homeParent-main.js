e2bb3a0ab569ef7042758f96463c68a7f90c1129
gerson22 <gerson.isaias.rivera@gmail.com>
2017-01-26 15:39:55 -0600

    cambios aqui

--- public/packages/assets/js/parent/homeParent-main.js
+++ public/packages/assets/js/parent/homeParent-main.js
@@ -1,5 +1,5 @@
 $(function(){
-   $(".carousel").carousel();
+
 
 	/* Transitions of the View ------------------------------------ */
 
@@ -15,44 +15,7 @@ $(function(){
 
 	}); // hide HELP MY SON
 
-        var ctx = document.getElementById("myChart").getContext("2d");
-        var data = {
-            labels: ["1ero", "2do", "3ero", "4to", "5to", "6to"],
-            datasets: [
-                {
-                    label: "Matemáticas",
-                    backgroundColor: "rgba(55, 106, 188, 0.4)",
-                    borderColor: "rgb(33, 86, 172)",
-                    pointBackgroundColor: "rgb(75, 120, 193)",
-                    pointBorderColor: "#fff",
-                    pointHoverBackgroundColor: "#fff",
-                    pointHoverBorderColor: "rgba(179,181,198,1)",
-                    data: [65, 59, 90, 81, 56, 60]
-                },
-                {
-                    label: "General",
-                    backgroundColor: "rgba(255,99,132,0.2)",
-                    borderColor: "rgba(255,99,132,1)",
-                    pointBackgroundColor: "rgba(255,99,132,1)",
-                    pointBorderColor: "#fff",
-                    pointHoverBackgroundColor: "#fff",
-                    pointHoverBorderColor: "rgba(255,99,132,1)",
-                    data: [80, 92, 94, 100, 90, 82]
-                }
-            ]
-        };
-        var options ={
-                scale: {
-                    reverse: false,
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }
-        }
-        var myChart = new Chart(ctx, {
-            type: 'radar',
-            data: data,
-            options: options
-        });
+
+
 
 });
