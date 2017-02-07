51ab3f1c6572a31b2efc52572e5fddae8c7c0b72
willy95 <wilvardo@gmail.com>
2017-02-06 19:34:52 -0600

    Corrigiendo link para administrar novedades de padres

--- app/views/templates/administer-menu.blade.php
+++ app/views/templates/administer-menu.blade.php
@@ -15,53 +15,57 @@
 @stop
 
 @section('menu-links')
-<div data-url="view-administer.admin-levels" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-levels" class="linkMenu waves-effect">
    <span class="fa fa-cube"></span></span>&nbsp;
    Niveles (Grados)
 </div>
-<div data-url="view-administer.admin-intelligences" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-intelligences" class="linkMenu waves-effect">
    <span class="fa fa-cube"></span></span>&nbsp;
    Inteligencias (Materias)
 </div>
-<div data-url="view-administer.admin-blocks" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-blocks" class="linkMenu waves-effect">
    <span class="fa fa-cube"></span></span>&nbsp;
    Bloques
 </div>
-<div data-url="view-administer.admin-topics" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-topics" class="linkMenu waves-effect">
    <span class="fa fa-cube"></span></span>&nbsp;
    Temas
 </div>
-<div data-url="view-administer.admin-activities" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-activities" class="linkMenu waves-effect">
    <span class="fa fa-cube"></span></span>&nbsp;
    Actividades (Juegos)
 </div>
-<div data-url="view-administer.asociateSchool" class="linkMenu waves-effect">
+<div data-url="/view-administer.asociateSchool" class="linkMenu waves-effect">
    <span class="fa fa-institution" id="msad-icon-institution"></span>&nbsp;
    Escuelas de apoyo
 </div>
-<div data-url="view-administer.admin-teachers" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-teachers" class="linkMenu waves-effect">
    <span class="fa fa-users" id="msad-icon-"></span>&nbsp;
    Profesores de apoyo
 </div>
-<div data-url="view-administer.admin-libraryPdfs" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-libraryPdfs" class="linkMenu waves-effect">
    <span class="fa fa-book" id="msad-icon-book"></span>&nbsp;
    Biblioteca de Pdfs
 </div>
-<div data-url="view-administer.admin-libraryVideos" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-libraryVideos" class="linkMenu waves-effect">
    <span class="fa fa-youtube-play" id="msad-icon-play"></span>&nbsp;
    Biblioteca de Videos
 </div>
-<div data-url="view-administer.admin-plans" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-plans" class="linkMenu waves-effect">
    <span class="fa fa-credit-card-alt" id="msad-icon-credit-card-alt"></span>&nbsp;
    Planes
 </div>
-<div data-url="view-administer.admin-employees" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-employees" class="linkMenu waves-effect">
    <span class="fa fa-address-book" id="msad-icon-fa-address-book"></span>&nbsp;
    Empleados
 </div>
-<div data-url="view-administer.admin-salescode" class="linkMenu waves-effect">
+<div data-url="/view-administer.admin-salescode" class="linkMenu waves-effect">
    <span class="fa fa-address-book" id="msad-icon-fa-address-book"></span>&nbsp;
    Códigos de vendedor
+</div>
+<div data-url="/view-dadNews/get/view-administer.admin-news" class="linkMenu waves-effect">
+   <span class="fa fa-star"></span>&nbsp;
+   Novedades para padres
 </div>
 @stop
 
@@ -113,6 +117,10 @@
 <div data-url="view-administer.admin-salescode" class="linkMenu linkMenuAside waves-effect">
    <span class="fa fa-address-book" id="msad-icon-fa-address-book"></span>&nbsp;
    Códigos de vendedor
+</div>
+<div data-url="view-administer.admin-news" class="linkMenu linkMenuAside waves-effect">
+   <span class="fa fa-star"></span>&nbsp;
+   Novedades para padres
 </div>
 <div data-url="#" class="linkMenu linkMenuAside waves-effect logOutBtn">
    <span class="fa fa-caret-right msad-icon-exit" id="msad-icon-exit"></span>&nbsp;
