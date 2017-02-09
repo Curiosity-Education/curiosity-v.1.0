bf992cd10d58c03dd65e9e3a8957f83a97307672
Tolu <jonathan.garcia@curiosity.com.mx>
2017-02-09 12:21:28 -0600

    cambios

--- public/packages/assets/js/parent/dispatchers/dp-child-registration.js
+++ public/packages/assets/js/parent/dispatchers/dp-child-registration.js
@@ -2,42 +2,48 @@ $(function(){
 
   var tempSons, tempId;
 
-  childRegistrationCrtrl.getSonsInfo(function(sons){
-    localStorage.localSons = JSON.stringify(sons);
+  childRegistrationCrtrl.getSons(function(sons){
+
+    sonsInfo(sons);
+
+    //Initializing carousel´s children
+    $(".carousel").carousel();
+    /*
+    |--------------------------------------------------------------------------
+    | managent of steps for registrate the children
+    |--------------------------------------------------------------------------
+    | in this section of code, we manage the steps for register the children
+    | show and hide steps
+    |
+    */
+
+    $(".carousel-item").on('click',function(){
+      tempId = $(this).data('id');
+    });
+
+    $("#upch-btnDelete").on('click',function(){
+  		childRegistrationCrtrl.delete(tempId);
+      $(".carousel").empty();
+      $(".carousel").on('load',function(){
+        $(".carousel").carousel();
+      })
+  	});
+
   });
 
-  tempSons = JSON.parse(localStorage.localSons);
+});
 
-  $.each(tempSons,function(i,o){
+function sonsInfo(object){
+  $.each(object,function(i,o){
     $.each(o,function(j,obj){
       $(".carousel").append($(
         "<a href='javascript:void(0)' class='carousel-item' data-id='" + obj.id + "'>" +
          "<div class='itemCarousel'>" +
-            "<img src='/packages/assets/media/images/parents/profile/mom-def.png'>" +
+            "<img src='" + obj.photoProfile + "'>" +
             "<h6 class='h6-responsive text-xs-center'>" + obj.nombre_completo + "</h6>" +
         "</div>" +
         "</a>"
       ));
     });
   });
-
-  //Initializing carousel´s children
-  $(".carousel").carousel();
-  /*
-  |--------------------------------------------------------------------------
-  | managent of steps for registrate the children
-  |--------------------------------------------------------------------------
-  | in this section of code, we manage the steps for register the children
-  | show and hide steps
-  |
-  */
-
-  $(".carousel-item").on('click',function(){
-    tempId = $(this).data('id');
-  });
-
-  $("#upch-btnDelete").on('click',function(){
-		childRegistrationCrtrl.delete(tempId);
-	});
-
-});
+}
