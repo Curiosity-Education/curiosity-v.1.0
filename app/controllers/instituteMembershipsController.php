e9f84f84fbd0ad01e35d5d50958fddc3c010a0e7
gerson22 <gerson.isaias.rivera@gmail.com>
2017-03-15 15:51:49 -0600

    Agregue el controlador de institucionesmembresias

---
+++ app/controllers/instituteMembershipsController.php
@@ -0,0 +1,29 @@
+<?php
+class instituteMembershipsController extends BaseController{
+    protected institute = null;
+    protected name = is_null($this->institute) ? 'inst' : $this->institute->nombre;
+    private function createUserName($folio){
+        /*
+        *    Nomenclature for username will be :
+        *
+        *    [name institute(The First 3 letters)] + [user-] + [folio (0-9)]
+        */
+        return substr($this->name,1,3).'user-'.$folio;
+    }
+    private function createPassUser(){
+        /*
+        *    Nomenclature for password will be :
+        *
+        *    [name institute(The First 3 letters)] + [folio (A-Z0-9){4}]
+        */
+        $cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        $lengthCadena=strlen($cadena);
+        $pass = substr($this->name,1,3);
+        $lengthFolio=4;
+        for($i=1 ; $i<=$lengthFolio ; $i++){
+            $pos=rand(0,$lengthCadena-1);
+            $pass .= substr($cadena,$pos,1);
+        }
+        return $pass;
+    }
+}
