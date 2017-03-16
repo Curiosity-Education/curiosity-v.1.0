87b1eca23e60ba7fddb7f425563a8670ffbd8bba
gerson22 <gerson.isaias.rivera@gmail.com>
2017-03-16 11:46:43 -0600

    Se cambio los keywords y description para mejorar el SEO

--- app/views/landing/indexBase.blade.php
+++ app/views/landing/indexBase.blade.php
@@ -5,8 +5,8 @@
 	<link rel="icon" type="image/png" href="/packages/assets/media/images/landing/logo.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta name="keywords" content="Curiosity, Educación,equidad, divertida,juegos,pdf, documentos, videos,retroalimentación,aventura,limites,evaluado,resultados, profesores">
-	<meta name="description" content="Curiosity educación  permite aprender con videojuegos educativos, apoyo de videos animados y documentos PDF con los temas vistos en clase">
+	<meta name="keywords" content="Curiosity, Educación, juegos, juegos educativos , juegos de primaria , tarea, escuela, videos para niños, videos animados">
+	<meta name="description" content="A través de juegos educativos y videos animados Curiosity Educación te ayuda en tus tareas de primaria">
 	<meta name="theme-color" content="#2262ae" >
 	<link rel="stylesheet" href="/packages/libs/mdb/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/packages/libs/mdb/css/mdb.min.css">
