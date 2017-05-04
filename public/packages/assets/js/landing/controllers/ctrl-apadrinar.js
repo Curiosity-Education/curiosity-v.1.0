d2d0dd0513b5008f7e4ca6f1f6b977ce8769d1a3
willy95 <wilvardo@gmail.com>
2017-05-04 10:41:01 -0500

    Redireccionando en mensaje de confirmacion de apadrinamiento

--- public/packages/assets/js/landing/controllers/ctrl-apadrinar.js
+++ public/packages/assets/js/landing/controllers/ctrl-apadrinar.js
@@ -133,7 +133,7 @@ var apadrinarController = {
                     'message' :
                     "<div class='text-justify'><center><img src='/packages/assets/media/images/parents/video.png' class='img-fluid' id='succImgPay'/><br /></center>"+
                     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div><br />"+
-                    "<div class='text-xs-right'><a href='/' class='btn btn-default' id='btnSuccPay'>Aceptar</a></div>",
+                    "<div class='text-xs-right'><a href='/casas-hogares' class='btn btn-default' id='btnSuccPay'>Aceptar</a></div>",
                     'icon': "fa-done"
                 };
                 Curiosity.windowMessage(notyBody.title, notyBody.message, notyBody.icon);
