1bac53486f9d997c0df47e7b5b7b3d2c61fc1b42
willy95 <wilvardo@gmail.com>
2017-05-29 11:34:00 -0500

    all

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'curiosity_db',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'admin',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
